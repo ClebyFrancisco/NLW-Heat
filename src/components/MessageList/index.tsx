6ae0fe74235ed39329b23000083b86b9c4bf7a23
Cleby Francisco <clebysilva5@gmail.com>
2021-10-21 23:40:42 -0300

    messageList

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,3 +1,54 @@
+import styles from "./styles.module.scss";
+
+import logoImg from "../../assets/logo.svg";
+
 export function MessageList() {
-  return <h1>MessageList</h1>;
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+            deserunt qui aut. Quam nihil dolorum nostrum
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/ClebyFrancisco.png" alt="" />
+            </div>
+            <span>Cleby Francisco</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+            deserunt qui aut. Quam nihil dolorum nostrum
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/ClebyFrancisco.png" alt="" />
+            </div>
+            <span>Cleby Francisco</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+            deserunt qui aut. Quam nihil dolorum nostrum
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/ClebyFrancisco.png" alt="" />
+            </div>
+            <span>Cleby Francisco</span>
+          </div>
+        </li>
+
+        
+      </ul>
+    </div>
+  );
 }
